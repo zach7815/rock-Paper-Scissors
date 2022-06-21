0115ffc481b06affb8b2cbff12f217d664f94fa1
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-06-21 21:21:49 +0100

    completed functionality for sudden death round feature, though it has a slight bug I need to fix

--- index.js
+++ index.js
@@ -9,22 +9,49 @@ const headings= {
  scoreDisplay:document.querySelectorAll(".scoreContainer"),
  scoreDisplayWrap:document.querySelector(".outterScoreWrap"),
  scoreCircle: document.createElement("div"),
- instrWrap: document.querySelector(".instrWrapper")
+ instrWrap: document.querySelector(".instrWrapper"),
+
+}
+const symbols = {
+ symbolCards: document.querySelectorAll(".symbolCard"),
+rockSymbol:{
+  rocContainer: document.querySelector(".rocCont"),
+rocImage: document.querySelector(".rockImg"),
+rocLabel: document.querySelector(".rockTitle"),
+},
+paperSymbol:{
+  papContainer: document.querySelector(".papCont"),
+papImage: document.querySelector(".papImg"),
+papLabel: document.querySelector(".papTitle"),
+},
+scissorsSymbol:{
+scissContainer: document.querySelector(".scisCont"),
+scissImage: document.querySelector(".scisImg"),
+scissLabel: document.querySelector(".scicTitle"),
 }
 
+}
+
+
 const animations = {
+  addZoom: ()=>{
+    symbols.symbolCards.forEach(element=>element.classList.add("symbolZoom"));
+  },
   selectSymbol: ()=>{
-    const symbol = document.querySelectorAll(".symbolCard");
-    symbol.forEach(element=>element.classList.add("symbolZoom"));
+    animations.addZoom()
     const detectUserChoice = (event) => {
       processUserChoice(event)
     }
     body.addEventListener('click', detectUserChoice);
-    symbol.forEach(element=>element.addEventListener("click", detectUserChoice))
+    symbols.symbolCards.forEach(element=>element.addEventListener("click", detectUserChoice))
+  },
+
+   removeZoom: ()=>{
+    symbols.symbolCards.forEach(element=>element.classList.remove("symbolZoom"));
   }
 }
 
-
+// removes initial event listeners for starting state of game. Avoids game reset
 function handler(e){
 interactions.forEach(e=> body.removeEventListener(e, handler))
 gameSequence()
@@ -155,61 +182,172 @@ const processUserChoice =(event)=>{
 const userChoice = event.target.id;
 const compChoice= generateCompAns();
 console.log(`The users choice selected was:${userChoice}`);
-console.log(` The computers choice selected was:${compChoice} `);
+console.log(` The computers choice selected was:${compChoice}`);
+
 calculateResult(userChoice,compChoice)
 }
 
-// adds animation to gameplay to make it more interactive.
-const gameAnimation = ()=> {
-const symbolImages = {
-  rock: "./images/rock.png",
-  paper:"./images/paper.png",
-  scissors: "./images/scissors.png"
-}
-// const transitionSlide= document.createElement("div");
-// transitionSlide.classList.add("transitionSlide");
-// body.prepend(transitionSlide);
-// transitionSlide.classList.add("transitionSlideFade")
-document.querySelector(".innerWrapper").style="gap:20rem"
-document.querySelector(".papCont").style="display:none"
 
 
-}
 
-// works out who wins comp or user
+// works out who wins comp or user and changes H2 to reflect result
 const calculateResult = (userChoice, compChoice)=>{
-  switch(userChoice){
 
+const displayDrawMes = ()=>{
+  setTimeout(()=>{
+    headings.instrWrap.firstChild.innerText="Ahh drat it's a draw!"
+   },7000)
+}
+const displayLossMes=()=>{
+  setTimeout(()=>{
+    headings.instrWrap.firstChild.innerText="Ahh no you lost!"
+   },7000)
+}
+const displayWinMes=()=>{
+  setTimeout(()=>{
+    headings.instrWrap.firstChild.innerText="Victory is yours!"
+   },7000)
+}
+  switch(userChoice){
     case "rock":
-    if(compChoice==="paper")
-    console.log("paper===loss")
-    else if(compChoice==="rock")
-    console.log("rock===draw")
-    else
-    console.log("scissors===win");
+      animations.removeZoom()
+      imageChangeAnimation()
+    if(compChoice==="paper"){
+     displayRoundResult(userChoice,compChoice);
+    displayLossMes();
+     
+   
+  } else if
+    (compChoice==="rock"){
+     displayRoundResult(userChoice,compChoice);
+      displayDrawMes()
+      
+    }
+    else{
+     displayRoundResult(userChoice,compChoice);
+     displayWinMes()
+     
+    }
     break
     
     case "paper":
+      animations.removeZoom()
+      imageChangeAnimation()
     if(compChoice==="paper")
-    console.log("paper===draw")
+     { displayRoundResult(userChoice,compChoice);
+      displayDrawMes()
+    }
+      
     else if(compChoice==="rock")
-    console.log("rock===win")
+     { displayRoundResult(userChoice,compChoice);
+      displayRoundResult(userChoice,compChoice);
+     displayWinMes()
+    
+    }
     else
-    console.log("scissors===loss");
+     { displayRoundResult(userChoice,compChoice);
+      displayLossMes()
+    }
+    break
+     
     break
     
     case "scissors":
+      animations.removeZoom()
+      imageChangeAnimation()
     if(compChoice==="paper")
-    console.log("paper===win")
+    {  displayRoundResult(userChoice,compChoice);
+     displayWinMes()
+      
+    
+    }
     else if(compChoice==="rock")
-    console.log("rock===loss")
+    {  displayRoundResult(userChoice,compChoice);
+      displayLossMes()
+    
+    }
     else
-    console.log("scissors===draw");
+    {  displayRoundResult(userChoice,compChoice);
+     displayLossMes()
+    
+    }
     break
     
     }
 }
 
+function imageChangeAnimation(){
+  
+  const imageCont= document.querySelectorAll(".symCont");
+   const symbol=document.querySelectorAll(".symImg");
+   const symbolLabel =document.querySelectorAll(".symHead");
+   symbols.scissorsSymbol.scissImage.src="./images/rock.png";
+   symbols.scissorsSymbol.scissLabel.innerText="rock";
+   symbols.paperSymbol.papContainer.classList.add("papContFade");
+    
+  
+   setTimeout(()=>{
+   bounceElement(imageCont);
+   setTimeout(()=>{
+     changeStyle(symbol, symbolLabel)  
+   },1000);
+     }
+  ,2000 )
+ }
+ 
+// Triggers round animation- Rock, Paper, Scissors, Shoot!
 
+ function bounceElement(element){
+ element.forEach(item => item.classList.add("animation"))
+ };
+ 
+ function changeStyle(images, imageLabel){
+
+     images.forEach(image=> image.src="./images/paper.png");
+         imageLabel.forEach(imageLabels=>imageLabels.innerText="paper");
+       setTimeout(()=>{
+     images.forEach(image=> image.src="./images/scissors.png");
+         imageLabel.forEach(imageLabels=>imageLabels.innerText="scissors");
+        setTimeout(()=>{
+          images.forEach(image=> image.style="display:none");
+  imageLabel.forEach(imageLabels=>imageLabels.innerText="shoot!");
+   },1000);
+       
+       
+   },1000);
+ 
+
+
+ }
+ 
+// sets image for comp and user choice after imageChange animation has run,
+// then adds reset button at bottom of the the screen. 
+
+ const displayRoundResult=(userChoice,compChoice)=>{
+
+  const symbolSrcs = {
+    rock:"./images/rock.png",
+    paper:"./images/paper.png",
+    scissors:"./images/scissors.png",
+  }
+
+  setTimeout(()=>{
+    symbols.rockSymbol.rocImage.src=symbolSrcs[userChoice];
+    symbols.rockSymbol.rocImage.style="display:block";
+    symbols.rockSymbol.rocLabel.innerText=userChoice;
+    symbols.scissorsSymbol.scissImage.src=symbolSrcs[compChoice];
+    symbols.scissorsSymbol.scissImage.style="display:block";
+    symbols.scissorsSymbol.scissLabel.innerText=compChoice;
+    const resetBtn = document.createElement("button");
+    resetBtn.innerText="Play Again?";
+  headings.instrWrap.append(resetBtn);
+  resetBtn.addEventListener("click", ()=>{
+    window.location.reload(false);
+  })
+  }, 7000);
+  
+
+
+ };
 
 
